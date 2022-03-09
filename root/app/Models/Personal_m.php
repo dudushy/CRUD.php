d666db4832a96cf127ec4f598cafb995c95cc308
dudushy <eduardotalarico1337@gmail.com>
2022-03-09 20:10:05 -0300

    Create Personal_m.php

---
+++ root/app/Models/Personal_m.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Personal_m extends Model
+{
+    protected $table = 'tb_personal';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['title', 'status', 'description'];
+}
