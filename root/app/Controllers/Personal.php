40c59ec2c0e2749e782ed28b1f54d64f02d1249f
dudushy <eduardotalarico1337@gmail.com>
2022-03-09 20:13:16 -0300

    Update Personal.php

--- root/app/Controllers/Personal.php
+++ root/app/Controllers/Personal.php
@@ -2,10 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\Personal_m;
+
 class Personal extends BaseController
 {
     public function index()
     {
-        return view('dashboard');
+        $model = new Personal_m();
+        $data = [
+                'personal' => $model -> findAll(),
+                'document_code' => 'document_code',
+        ];
+        return view('dashboard', $data);
     }
 }
