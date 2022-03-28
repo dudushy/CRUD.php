e60f902d74d00b8f3e0b260ef0127b6e8f1cf9bb
dudushy <eduardotalarico1337@gmail.com>
2022-03-28 19:02:11 -0300

    Update crud_ci3.sql

--- crud_ci3.sql
+++ crud_ci3.sql
@@ -1,49 +1,104 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 29, 2022 at 12:00 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema crud_ci3
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema crud_ci3
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `crud_ci3` DEFAULT CHARACTER SET utf8 ;
-USE `crud_ci3` ;
-
--- -----------------------------------------------------
--- Table `crud_ci3`.`perfil`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `crud_ci3`.`perfil` (
-  `per_id` INT NOT NULL AUTO_INCREMENT,
-  `per_nombre` VARCHAR(50) NULL,
-  PRIMARY KEY (`per_id`))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `crud_ci3`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `crud_ci3`.`usuario` (
-  `usu_id` INT NOT NULL AUTO_INCREMENT,
-  `per_id` INT NOT NULL,
-  `usu_nombres` VARCHAR(50) NULL,
-  `usu_apellidos` VARCHAR(100) NULL,
-  `usu_correo` VARCHAR(50) NULL,
-  `usu_telefono` VARCHAR(9) NULL,
-  PRIMARY KEY (`usu_id`),
-  INDEX `fk_usuario_perfil_idx` (`per_id` ASC),
-  CONSTRAINT `fk_usuario_perfil`
-    FOREIGN KEY (`per_id`)
-    REFERENCES `crud_ci3`.`perfil` (`per_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `crud_ci3`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_profile`
+--
+
+CREATE TABLE `tb_profile` (
+  `prf_id` int(11) NOT NULL,
+  `prf_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_profile`
+--
+
+INSERT INTO `tb_profile` (`prf_id`, `prf_name`) VALUES
+(1, 'Administrador'),
+(2, 'Vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `usr_id` int(11) NOT NULL,
+  `prf_id` int(11) NOT NULL,
+  `usr_name` varchar(50) DEFAULT NULL,
+  `usr_nickname` varchar(100) DEFAULT NULL,
+  `usr_email` varchar(50) DEFAULT NULL,
+  `usr_telephone` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_profile`
+--
+ALTER TABLE `tb_profile`
+  ADD PRIMARY KEY (`prf_id`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`usr_id`),
+  ADD KEY `fk_user_profile_idx` (`prf_id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_profile`
+--
+ALTER TABLE `tb_profile`
+  MODIFY `prf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `fk_usuario_perfil` FOREIGN KEY (`prf_id`) REFERENCES `tb_profile` (`prf_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
